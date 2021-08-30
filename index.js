509ea3105456fde53d44e6730db20f8be940ae60
Damian A. Pastorini <damian.pastorini>
2021-08-30 15:51:49 +0200

    - Reldens - Storage

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * Reldens - Storage
+ *
+ * This module handle the database connection and queries.
+ *
+ */
+
+const { BaseDataServer } = require('./lib/base-data-server');
+const { BaseDriver } = require('./lib/base-driver');
+const { ObjectionJsDriver } = require('./lib/objection-js-driver');
+const { ObjectionJsDataServer } = require('./lib/objection-js-data-server');
+const { MikroOrmDriver } = require('./lib/mikro-orm-driver');
+const { ModelClass } = require('./lib/objection-js-model');
+
+module.exports = {
+    ModelClass: ModelClass,
+    // databases:
+    BaseDataServer: BaseDataServer,
+    ObjectionJsDataServer: ObjectionJsDataServer,
+    // drivers:
+    BaseDriver: BaseDriver,
+    ObjectionJsDriver: ObjectionJsDriver,
+    MikroOrmDriver: MikroOrmDriver
+};
